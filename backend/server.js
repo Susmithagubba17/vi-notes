a840bcd9ca7424256b3d50acb92fe5bb3caa6e6a
Susmithagubba17 <sushmithagubba1709@gmail.com>
2026-04-06 19:08:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -5,9 +6,9 @@ const app = express();
 app.use(express.json());
 
 // Connect MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/typingApp')
-.then(() => console.log("✅ DB Connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 // Test route
 //app.get('/', (req, res) => {
